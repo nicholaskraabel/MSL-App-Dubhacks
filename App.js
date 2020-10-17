103968a8e750f3df9de35b22db6db64f43a64a16
nicholaskraabel <nrkraabel4@gmail.com>
2020-10-17 15:44:58 -0700

    Revert "rename"
    This reverts commit 30704f892f3c3943f91ef8c87cf9b660c6ddaa6e.

--- App.js
+++ App.js
@@ -1,28 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { Audio } from 'expo-av';
-import * as Speech from 'expo-speech';
-import { render } from 'react-dom';
+import { StyleSheet, Text, View } from 'react-native';
 
-
-export default class elementSP extends React.Component{
-    speak() {
-        Speech.speak("Hi does this work hopefully");
-    }
-    render() {
-        return(
-        <View style={styles.addlater}>
-            <Button title="testsound" onPress={this.speak}/>
-        </View>
-        );
-    }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello all</Text>
+      <Text>More text to not conflict</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    addlater:{
-        flex: 1,
-        backgroundColor: '#ecf2f0'
-    }
-
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
