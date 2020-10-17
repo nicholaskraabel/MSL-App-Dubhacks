e819aad752f030396caeb73ea1db66adba227eac
nicholaskraabel <nrkraabel4@gmail.com>
2020-10-17 14:23:13 -0700

    testSpeech

--- ReadActPhoneticSound.js
+++ ReadActPhoneticSound.js
@@ -1,3 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { Audio } from 'expo-av';
+import * as Speech from 'expo-speech';
+import { render } from 'react-dom';
+
+
+export default class App extends React.Component{
+    speak() {
+        Speech.speak("Hi does this work hopefully");
+    }
+    render() {
+        return(
+        <View style={styles.addlater}>
+            <Button title="testsound" onPress={this.speak}/>
+        </View>
+        );
+    }
+}
+
+export default NativeSpeechtest
+
+const styles = StyleSheet.create({
+    addlater:{
+        flex: 1,
+        backgroundColor: '#ecf2f0'
+    }
+
+});
