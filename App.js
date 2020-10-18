d943297e545be56d1efd026bc2a6f55ff839b0b0
Noah Y Nguyen <yngt.noah@gmail.com>
2020-10-18 02:34:01 -0700

    test

--- App.js
+++ App.js
@@ -10,12 +10,10 @@ import AppNavigator from "./assets/navigation/AppNavigator";
 
 import ReadAct from './ReadActPhoneticSound';
 
-
+import Writting from './WritingAct';
 export default function App() {
   return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator/>
-    </NavigationContainer>
+   <Writting/>
   );
 }
 
