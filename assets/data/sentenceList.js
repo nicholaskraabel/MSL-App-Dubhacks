318e5ab5d6bc49c58098635efdeb73d726616f48
Anthony Reyes <anthonyreyesf@gmail.com>
2020-10-18 00:35:17 -0400

    Minor changes + added json file

---
+++ assets/data/sentenceList.js
@@ -0,0 +1,761 @@
+[
+    {
+      "Sentence": "The cat stretched."
+    },
+    {
+      "Sentence": "Jacob stood on his tiptoes."
+    },
+    {
+      "Sentence": "The car turned the corner."
+    },
+    {
+      "Sentence": "Kelly twirled in circles."
+    },
+    {
+      "Sentence": "She opened the door."
+    },
+    {
+      "Sentence": "Aaron made a picture."
+    },
+    {
+      "Sentence": "I'm sorry."
+    },
+    {
+      "Sentence": "I danced."
+    },
+    {
+      "Sentence": "Run!"
+    },
+    {
+      "Sentence": "Open the jar carefully."
+    },
+    {
+      "Sentence": "Read the directions."
+    },
+    {
+      "Sentence": "Don't cry."
+    },
+    {
+      "Sentence": "Use common sense."
+    },
+    {
+      "Sentence": "Make the best of things."
+    },
+    {
+      "Sentence": "Catch up!"
+    },
+    {
+      "Sentence": "Misha walked and looked around."
+    },
+    {
+      "Sentence": "My mother hemmed and hawed over where to go for dinner."
+    },
+    {
+      "Sentence": "He was eating and talking."
+    },
+    {
+      "Sentence": "I rinsed and dried the dishes."
+    },
+    {
+      "Sentence": "Joe stood up and spoke to the crowd."
+    },
+    {
+      "Sentence": "The leaves turn orange and yellow in the fall."
+    },
+    {
+      "Sentence": "The fan cools the air in the room."
+    },
+    {
+      "Sentence": "Every summer he visited his grandparents on the farm."
+    },
+    {
+      "Sentence": "You can tell that the horse is fast."
+    },
+    {
+      "Sentence": "The feather is light."
+    },
+    {
+      "Sentence": "The fence went around the land."
+    },
+    {
+      "Sentence": "The baby is grabbing her finger."
+    },
+    {
+      "Sentence": "Let's cook hot dogs over the fire."
+    },
+    {
+      "Sentence": "She caught a fish that was bigger than a dog."
+    },
+    {
+      "Sentence": "He pounded his fist on the table."
+    },
+    {
+      "Sentence": "He ran in lane five."
+    },
+    {
+      "Sentence": "She held the babies tiny foot."
+    },
+    {
+      "Sentence": "The boys enjoyed playing football every Saturday."
+    },
+    {
+      "Sentence": "Many trees are in the forest."
+    },
+    {
+      "Sentence": "You will need a fork and knife to eat dinner."
+    },
+    {
+      "Sentence": "She will be four years old in October."
+    },
+    {
+      "Sentence": "What does the fox say?"
+    },
+    {
+      "Sentence": "Give me a phone call in 10 minutes."
+    },
+    {
+      "Sentence": "atie can say the letters of the alphabet in order."
+    },
+    {
+      "Sentence": "I love bacon, eggs, and orange juice for breakfast."
+    },
+    {
+      "Sentence": "We saw buffalo at Yellowstone National Park."
+    },
+    {
+      "Sentence": "We caught six catfish in the pond."
+    },
+    {
+      "Sentence": "This coffee is too hot!"
+    },
+    {
+      "Sentence": "The dolphin smiled at me."
+    },
+    {
+      "Sentence": "The elephant is washing himself."
+    },
+    {
+      "Sentence": "The goldfish has a white tail."
+    },
+    {
+      "Sentence": "He put the golf ball on the tee."
+    },
+    {
+      "Sentence": "Turn the music down in your headphones."
+    },
+    {
+      "Sentence": "The lifeguard blew his whistle."
+    },
+    {
+      "Sentence": "Turn on your microphone."
+    },
+    {
+      "Sentence": "That chocolate muffin looks good."
+    },
+    {
+      "Sentence": "The office workers were all gone."
+    },
+    {
+      "Sentence": "I have played the saxophone for three years."
+    },
+    {
+      "Sentence": "There is heavy traffic on the highway."
+    },
+    {
+      "Sentence": "He won a trophy for his science project."
+    },
+    {
+      "Sentence": "I want waffles for breakfast."
+    },
+    {
+      "Sentence": "The baby calf was close to its mother."
+    },
+    {
+      "Sentence": "The chef made the best pasta."
+    },
+    {
+      "Sentence": "The Indian chief was the leader."
+    },
+    {
+      "Sentence": "Be careful, you don't want to fall off the cliff."
+    },
+    {
+      "Sentence": "I have a cough and a runny nose."
+    },
+    {
+      "Sentence": "My shirt cuff got food on it during lunch."
+    },
+    {
+      "Sentence": "The elf was a beautiful princess."
+    },
+    {
+      "Sentence": "The giraffe has a long neck."
+    },
+    {
+      "Sentence": "We ate half of the pizza."
+    },
+    {
+      "Sentence": "A horse's foot is called a hoof."
+    },
+    {
+      "Sentence": "The knife is made of metal."
+    },
+    {
+      "Sentence": "I laugh every time I see him on TV."
+    },
+    {
+      "Sentence": "The leaf fell from the tree."
+    },
+    {
+      "Sentence": "Turn off the TV and go to bed."
+    },
+    {
+      "Sentence": "The roof was made of clay tiles."
+    },
+    {
+      "Sentence": "We have colorful books on our shelf."
+    },
+    {
+      "Sentence": "The sheriff took the robber to jail."
+    },
+    {
+      "Sentence": "All we could hear was a wolf howling."
+    },
+    {
+      "Sentence": "You are going too fast."
+    },
+    {
+      "Sentence": "Come with me."
+    },
+    {
+      "Sentence": "Good afternoon."
+    },
+    {
+      "Sentence": "Good morning."
+    },
+    {
+      "Sentence": "Good night."
+    },
+    {
+      "Sentence": "Have a good trip."
+    },
+    {
+      "Sentence": "Have a good weekend."
+    },
+    {
+      "Sentence": "I admire you."
+    },
+    {
+      "Sentence": "I apologize."
+    },
+    {
+      "Sentence": "I can’t wait."
+    },
+    {
+      "Sentence": "I don’t have time."
+    },
+    {
+      "Sentence": "I got it."
+    },
+    {
+      "Sentence": "I hate to see you so sad."
+    },
+    {
+      "Sentence": "I hope so."
+    },
+    {
+      "Sentence": "I knew it."
+    },
+    {
+      "Sentence": "I love you."
+    },
+    {
+      "Sentence": "Let’s catch up!"
+    },
+    {
+      "Sentence": "Let’s do it."
+    },
+    {
+      "Sentence": "Nice to meet you."
+    },
+    {
+      "Sentence": "It is polite to shake hands."
+    },
+    {
+      "Sentence": "She bought a cut, color, and shampoo."
+    },
+    {
+      "Sentence": "She is nice to share her ice cream."
+    },
+    {
+      "Sentence": "Be careful the thorn is sharp."
+    },
+    {
+      "Sentence": "I shave my face in the morning."
+    },
+    {
+      "Sentence": "The sheep is standing in the grass."
+    },
+    {
+      "Sentence": "He gives them a good shine every day."
+    },
+    {
+      "Sentence": "The ship will be gone for 10 days."
+    },
+    {
+      "Sentence": "He wore his gray shirt."
+    },
+    {
+      "Sentence": "This type of shoe is for hiking."
+    },
+    {
+      "Sentence": "I need to shop for groceries."
+    },
+    {
+      "Sentence": "The doctor needs to give you a shot."
+    },
+    {
+      "Sentence": "They massaged his sore shoulder."
+    },
+    {
+      "Sentence": "He went to shovel his driveway."
+    },
+    {
+      "Sentence": "She washes her hair in the shower."
+    },
+    {
+      "Sentence": "She is shy around new people."
+    },
+    {
+      "Sentence": "Don't put too much sugar in the lemonade."
+    },
+    {
+      "Sentence": "The detective listened to her tales with a wooden face."
+    },
+    {
+      "Sentence": "She was fairly certain that life was a fashion show."
+    },
+    {
+      "Sentence": "The typical teenage boy’s room is a disaster area."
+    },
+    {
+      "Sentence": "What storms then shook the ocean of my sleep."
+    },
+    {
+      "Sentence": "The children were roses grown in concrete gardens, beautiful and forlorn."
+    },
+    {
+      "Sentence": "Kisses are the flowers of love in bloom."
+    },
+    {
+      "Sentence": "His cotton candy words did not appeal to her taste."
+    },
+    {
+      "Sentence": "Kathy arrived at the grocery store with an army of children."
+    },
+    {
+      "Sentence": "Her eyes were fireflies."
+    },
+    {
+      "Sentence": "He wanted to set sail on the ocean of love but he just wasted away in the desert."
+    },
+    {
+      "Sentence": "I was lost in a sea of nameless faces."
+    },
+    {
+      "Sentence": "John’s answer to the problem was just a Band-Aid, not a solution."
+    },
+    {
+      "Sentence": "The cast on Michael’s broken leg was a plaster shackle."
+    },
+    {
+      "Sentence": "Cameron always had a taste for the fruit of knowledge."
+    },
+    {
+      "Sentence": "The promise between us was a delicate flower."
+    },
+    {
+      "Sentence": "He’s a rolling stone, and it’s bred in the bone."
+    },
+    {
+      "Sentence": "He pleaded for her forgiveness but Janet’s heart was cold iron."
+    },
+    {
+      "Sentence": "She was just a trophy to Ricardo, another object to possess."
+    },
+    {
+      "Sentence": "The path of resentment is easier to travel than the road to forgiveness."
+    },
+    {
+      "Sentence": "Katie’s plan to get into college was a house of cards on a crooked table."
+    },
+    {
+      "Sentence": "The wheels of justice turn slowly."
+    },
+    {
+      "Sentence": "Hope shines–a pebble in the gloom."
+    },
+    {
+      "Sentence": "She cut him down with her words."
+    },
+    {
+      "Sentence": "The job interview was a rope ladder dropped from heaven."
+    },
+    {
+      "Sentence": "Her hair was a flowing golden river streaming down her shoulders."
+    },
+    {
+      "Sentence": "The computer in the classroom was an old dinosaur."
+    },
+    {
+      "Sentence": "The clouds were as big as an elephant."
+    },
+    {
+      "Sentence": "Anna is as sly as a fox."
+    },
+    {
+      "Sentence": "Brian’s face was as clear as day."
+    },
+    {
+      "Sentence": "Carson’s hair is as dark as the night."
+    },
+    {
+      "Sentence": "Her skin is as dry as a bone."
+    },
+    {
+      "Sentence": "Her attitude towards him is as cold as ice."
+    },
+    {
+      "Sentence": "Maria’s schedule makes her as busy as a bee."
+    },
+    {
+      "Sentence": "Martin’s wit is as sharp as a needle."
+    },
+    {
+      "Sentence": "Jerry’s grandfather is as tough as an old boot."
+    },
+    {
+      "Sentence": "The test questions were as easy as a pie."
+    },
+    {
+      "Sentence": "My father’s love for my mother is as deep as the ocean."
+    },
+    {
+      "Sentence": "The office is as clean as a whistle."
+    },
+    {
+      "Sentence": "She is as strong as an ox"
+    },
+    {
+      "Sentence": "They were as different as salt and pepper but they are still together."
+    },
+    {
+      "Sentence": "The troops sent to the war were as brave as lions."
+    },
+    {
+      "Sentence": "Sarah walked to class, but Kevin ran."
+    },
+    {
+      "Sentence": "I want hamburgers, but Lois wants pizza."
+    },
+    {
+      "Sentence": "The photographer held up a bear, and the little boy smiled."
+    },
+    {
+      "Sentence": "I fell out of the bed, so Mom came to check on me."
+    },
+    {
+      "Sentence": "The dog buried his bone, and the cat sniffed around it."
+    },
+    {
+      "Sentence": "I was late; however, the class had not started."
+    },
+    {
+      "Sentence": "I left my bag on the bus; therefore, I did not have a pencil."
+    },
+    {
+      "Sentence": "Can I go home with you, so we can do our homework together?"
+    },
+    {
+      "Sentence": "It was very hot outside, and the ice cream melted."
+    },
+    {
+      "Sentence": "Our team tried hard, but we lost the game."
+    },
+    {
+      "Sentence": "They live in a beautiful house."
+    },
+    {
+      "Sentence": "Lisa is wearing a sleeveless shirt today."
+    },
+    {
+      "Sentence": "She wore a beautiful dress."
+    },
+    {
+      "Sentence": "He writes meaningless letters."
+    },
+    {
+      "Sentence": "This shop is much nicer."
+    },
+    {
+      "Sentence": "She wore a beautiful dress."
+    },
+    {
+      "Sentence": "Ben is an adorable baby."
+    },
+    {
+      "Sentence": "Linda’s hair is gorgeous."
+    },
+    {
+      "Sentence": "This glass is breakable."
+    },
+    {
+      "Sentence": "I met a homeless person in NY."
+    },
+    {
+      "Sentence": "There’s a fire inside the fire station."
+    },
+    {
+      "Sentence": "What a cute puppy!"
+    },
+    {
+      "Sentence": "That birthday cake was so good!"
+    },
+    {
+      "Sentence": "Sheldon's gift was so amazing!"
+    },
+    {
+      "Sentence": "Eugh, that bug is so ugly!"
+    },
+    {
+      "Sentence": "I'm so mad right now!"
+    },
+    {
+      "Sentence": "He loves to play basketball."
+    },
+    {
+      "Sentence": "He goes to school."
+    },
+    {
+      "Sentence": "Does he go to school?"
+    },
+    {
+      "Sentence": "She writes an e-mail to her best friend."
+    },
+    {
+      "Sentence": "He thinks he is very handsome."
+    },
+    {
+      "Sentence": "It usually rains every day here."
+    },
+    {
+      "Sentence": "It smells very delicious in the kitchen."
+    },
+    {
+      "Sentence": "We generally sing songs all together."
+    },
+    {
+      "Sentence": "We go to a gallery every Sunday."
+    },
+    {
+      "Sentence": "Does he write an email?"
+    },
+    {
+      "Sentence": "The sun rises at the east."
+    },
+    {
+      "Sentence": "She goes to work by car."
+    },
+    {
+      "Sentence": "It doesn’t rain here in the summer."
+    },
+    {
+      "Sentence": "We cook every day."
+    },
+    {
+      "Sentence": "We go to the gym club together."
+    },
+    {
+      "Sentence": "You have a big house."
+    },
+    {
+      "Sentence": "Do we know each other?"
+    },
+    {
+      "Sentence": "They sleep in the afternoon."
+    },
+    {
+      "Sentence": "When do they usually talk to each other?"
+    },
+    {
+      "Sentence": "The children are at home."
+    },
+    {
+      "Sentence": "The earth goes round the sun."
+    },
+    {
+      "Sentence": "George brushes his teeth twice a day."
+    },
+    {
+      "Sentence": "He gets up early every day."
+    },
+    {
+      "Sentence": "They speak English in USA."
+    },
+    {
+      "Sentence": "I like reading detective stories."
+    },
+    {
+      "Sentence": "I like geography and science."
+    },
+    {
+      "Sentence": "She doesn’t study German on Monday."
+    },
+    {
+      "Sentence": "Does she live in Paris?"
+    },
+    {
+      "Sentence": "He doesn’t teach math."
+    },
+    {
+      "Sentence": "Cats hate water."
+    },
+    {
+      "Sentence": "Every child likes an icecream."
+    },
+    {
+      "Sentence": "My mother never lies."
+    },
+    {
+      "Sentence": "The Earth is spherical."
+    },
+    {
+      "Sentence": "She doesn’t use a computer."
+    },
+    {
+      "Sentence": "It snows a lot in winter in Russia."
+    },
+    {
+      "Sentence": "We live in Texas."
+    },
+    {
+      "Sentence": "You go to holiday every summer."
+    },
+    {
+      "Sentence": "Do you like spaghetti?"
+    },
+    {
+      "Sentence": "My daughter does the laundry."
+    },
+    {
+      "Sentence": "My brother takes out the trash."
+    },
+    {
+      "Sentence": "The course starts next Sunday."
+    },
+    {
+      "Sentence": "She swims every morning."
+    },
+    {
+      "Sentence": "I don’t wash the dishes."
+    },
+    {
+      "Sentence": "We see them every week."
+    },
+    {
+      "Sentence": "I don’t like tea."
+    },
+    {
+      "Sentence": "When does the train usually leave?"
+    },
+    {
+      "Sentence": "She always forgets her purse."
+    },
+    {
+      "Sentence": "You don’t have children."
+    },
+    {
+      "Sentence": "I and my sister don’t see each other anymore."
+    },
+    {
+      "Sentence": "They don’t go to school tomorrow."
+    },
+    {
+      "Sentence": "She wants to be a dentist."
+    },
+    {
+      "Sentence": "Cows eat grass."
+    },
+    {
+      "Sentence": "My cat runs very fast."
+    },
+    {
+      "Sentence": "She has a beehive full of bees."
+    },
+    {
+      "Sentence": "My son lives in London."
+    },
+    {
+      "Sentence": "They don’t have any money."
+    },
+    {
+      "Sentence": "She plays basketball."
+    },
+    {
+      "Sentence": "He catches the train every morning."
+    },
+    {
+      "Sentence": "My sister works at the theater."
+    },
+    {
+      "Sentence": "Michael doesn’t work."
+    },
+    {
+      "Sentence": "How often do you see George?"
+    },
+    {
+      "Sentence": "She doesn’t see Peter every day."
+    },
+    {
+      "Sentence": "My boyfriend loves this song."
+    },
+    {
+      "Sentence": "My father doesn’t speak good English."
+    },
+    {
+      "Sentence": "He goes to football every day."
+    },
+    {
+      "Sentence": "California is not in the United Kingdom."
+    },
+    {
+      "Sentence": "The Queen of England lives in Buckingham Palace."
+    },
+    {
+      "Sentence": "Julie talks very fast."
+    },
+    {
+      "Sentence": "My brother’s dog barks a lot."
+    },
+    {
+      "Sentence": "Does he play tennis?"
+    },
+    {
+      "Sentence": "The train leaves every morning."
+    },
+    {
+      "Sentence": "Water freezes at 0°C."
+    },
+    {
+      "Sentence": "I love my new pets."
+    },
+    {
+      "Sentence": "We drink coffee every morning."
+    },
+    {
+      "Sentence": "My Dad never works on the weekends."
+    },
+    {
+      "Sentence": "She doesn’t teach chemistry."
+    }
+  ]
