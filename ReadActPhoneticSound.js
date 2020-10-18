edff737da7d9922be77961c20f92ce7dc3217052
nicholaskraabel <nrkraabel4@gmail.com>
2020-10-17 22:04:35 -0700

    check out

--- ReadActPhoneticSound.js
+++ ReadActPhoneticSound.js
@@ -1,45 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button, useState } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button, useState, Alert } from 'react-native';
 import { Audio } from 'expo-av';
 import * as Speech from 'expo-speech';
 import { render } from 'react-dom';
-import * as XLSX from 'xlsx';
-import DataTable from 'react-data-table-component';
+import styles from './assets/styles/stylesheet.js'
 
+var dataArray = ["some sentences", "this better function", "I really hope this works"];
+export default class SpeechTask extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {textArray: ["some sentences", "this better function", "I really hope this works"],
+        text: "starting phrase",
+        num: 0}
 
-$.ajax({
-    url: "pathto/filename.csv",
-    async: false,
-    success: function (csvd) {
-        data = $.csv.toArrays(csvd);
-    },
-    dataType: "text",
-    complete: function () {
-        // call a function on complete 
-    }
-});
-
-export default class elementSP extends React.Component{
-    speak() {
-        Speech.speak("Hi does this work hopefully");
-    }
-
-    render() {
-        return(
-        <View style={styles.addlater}>
-            <Button title="testsound" onPress={this.speak}/>
-        </View>
-        );
-    }
-
-    
+const clickHandler = () => {
+    num++;
+    let text = textArra[num];
+    this.setState({text})
 }
 
+render(){
+return(
+    <View style={styles.h1}>
+        <Text>{text}</Text>
+        <View style={styles.button}>
+            <Button style={styles.buttonText} title = "Next" onPress={clickHandler}/>
+        </View>
+    </View>
+);
+}
 
-const styles = StyleSheet.create({
-    addlater:{
-        flex: 1,
-        backgroundColor: '#ecf2f0'
-    }
-
-});
+}
