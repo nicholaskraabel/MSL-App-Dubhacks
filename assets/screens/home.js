45ff6f0be64d06cd1cdbc855628b948743e2228b
Francisco Lopez <franktheballer23@gmail.com>
2020-10-17 23:58:32 -0700

    removing safe-area content

--- assets/screens/home.js
+++ assets/screens/home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { View, Image } from 'react-native';
 import H1 from '../components/h1';
 import Styles from '../styles/stylesheet';
 
