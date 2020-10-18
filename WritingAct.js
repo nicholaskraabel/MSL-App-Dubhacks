e31e4bf8e2039ab26b2943eb93526d4ceb0b1d62
Noah Y Nguyen <yngt.noah@gmail.com>
2020-10-18 02:37:24 -0700

    test

--- WritingAct.js
+++ WritingAct.js
@@ -1,5 +1,7 @@
+import { Video } from 'expo-av';
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
+import { TextInput, StyleSheet, View, Text } from 'react-native';
+
 
 //generate random sentence
 var sentences = [
@@ -369,16 +371,31 @@ window.alert(random_sentence(sentences));
   
 
 //user typing box
-const UserTextInput = () => {
+function UserTextInput () {
   const [value, onChangeText] = React.useState('Type in what you see');
 
   return (
-    <TextInput
-      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-      onChangeText={text => onChangeText(text)}
-      value={value}
-    />
+    <View style={styles.container}>
+      <TextInput
+        style={{ flex: 0.5, height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={text => onChangeText(text)}
+        value={value}
+      />
+      <Text>{value}</Text>
+    </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container:{
+      flex: 1,
+     
+      alignContent: "center",
+      padding: 50,
+  },
+
+});
+
 
 export default UserTextInput;
